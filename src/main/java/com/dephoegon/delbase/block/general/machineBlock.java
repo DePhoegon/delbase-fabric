76cd40c9534e3916cc30e4734c23585810c65c6e
Kerns Noel(Acer) <45953503+DePhoegon@users.noreply.github.com>
2023-09-21 10:26:19 -0600

    1.5.3 -version fixed ToolTip
    Signed-off-by: Kerns Noel(Acer) <45953503+DePhoegon@users.noreply.github.com>
    
    Took 28 minutes

--- src/main/java/com/dephoegon/delbase/block/general/machineBlock.java
+++ src/main/java/com/dephoegon/delbase/block/general/machineBlock.java
@@ -6,17 +6,9 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
-import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class machineBlock {
     public static final Block BLOCK_CUTTER_BLOCK = registerBlock(
@@ -29,12 +21,7 @@ public class machineBlock {
 
     private static void registerBlockItem(Block block) {
         Registry.register(Registry.ITEM, new Identifier(Delbase.Delbase_ID, "block_cutting_station"),
-                new BlockItem(block, new FabricItemSettings().group(Delbase.DELBASE_ITEMS)) {
-                    @Override
-                    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-                        tooltip.add(new TranslatableText("block.delbase.block_cutting_station_tooltip"));
-                    }
-                });
+                new BlockItem(block, new FabricItemSettings().group(Delbase.DELBASE_ITEMS)));
     }
     public static void registerModBlocks() {
         Delbase.LOGGER.info("Registering MachineBlocks for " + Delbase.Delbase_ID);
