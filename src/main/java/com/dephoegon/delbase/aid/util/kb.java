be44d6910c2051bb99a0a451badbbd7b0cc9ffca
Kerns Noel (ASRock) <45953503+DePhoegon@users.noreply.github.com>
2025-05-30 16:52:11 -0600

    KeyBind Touchup
    Cleared up  Kb & Key binding classes
    
    Took 7 minutes

--- src/main/java/com/dephoegon/delbase/aid/util/kb.java
+++ src/main/java/com/dephoegon/delbase/aid/util/kb.java
@@ -4,23 +4,17 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 
-import static com.dephoegon.delbase.aid.util.ModKeyBindings.L_CTRL;
-import static com.dephoegon.delbase.aid.util.ModKeyBindings.L_SHIFT;
+import static com.dephoegon.delbase.aid.util.ModKeyBindings.R_CTRL;
+import static com.dephoegon.delbase.aid.util.ModKeyBindings.R_SHIFT;
 import static net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper.getBoundKeyOf;
 import static net.minecraft.client.util.InputUtil.isKeyPressed;
 
 public class kb {
     //  Right ones commented code out. 1.18.2 is just ...   IDK, I've no luck with keybinding conflicts - will keep till I decide how to handle the rest of the versions.
-    private static boolean LShift() { return isKB_KeyBindDown(L_SHIFT); }
-    //private static boolean RShift() { return keyCheck(GLFW.GLFW_KEY_RIGHT_SHIFT); }
-    private static boolean LCtrl() { return isKB_KeyBindDown(L_CTRL); }
-    // private static boolean RCtrl() { return keyCheck(GLFW.GLFW_KEY_RIGHT_CONTROL); }
-    /*
-    public static boolean HShift() { return isKeyBindDefault(L_SHIFT) ? LShift() || RShift() : LShift(); }
-    public static boolean HCtrl() { return isKeyBindDefault(L_CTRL) ? LCtrl() || RCtrl() : LCtrl(); }
-     */
-    public static boolean HShift() { return LShift(); }
-    public static boolean HCtrl() { return LCtrl(); }
+    private static boolean RShift() { return isKB_KeyBindDown(R_SHIFT); }
+    private static boolean RCtrl() { return isKB_KeyBindDown(R_CTRL); }
+    public static boolean HShift() { return RShift(); }
+    public static boolean HCtrl() { return RCtrl(); }
 
     public static boolean isKB_KeyBindDown(KeyBinding mapping) {
         if (mapping == null) { return false; }
@@ -28,15 +22,8 @@ public class kb {
         int Keycode = key.getCode();
         return keyCheck(key.getCategory(), Keycode);
     }
-    // private static boolean keyCheck (int Keycode) { return keyCheck(InputUtil.Type.KEYSYM, Keycode); }
     public static boolean keyCheck(InputUtil.Type type, int Keycode) {
         if (type != InputUtil.Type.KEYSYM) { return false; }
         return isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), Keycode);
     }
-    /*
-    public static boolean isKeyBindDefault(KeyBinding mapping) {
-        if (mapping != null) { return mapping.isDefault(); }
-        return false;
-    }
-     */
 }
